83046460a8e41d2e860776368c76b872f0dc7d8a
ganeshkalyank <srikgk333@gmail.com>
2023-02-03 18:39:22 +0530

    added outputs for sorting algos

--- Sorting/random_feed.cpp
+++ Sorting/random_feed.cpp
@@ -6,7 +6,9 @@ int main() {
 	srand((long int) clock());
 	ofstream file("input.txt");
 	for (int i=0;i<11000;i++) {
-		file<<rand()%30000<<" ";
+		// file<<rand()%30000<<" ";
+		// file << i << " ";
+		file << 11000 - i << " ";
 	}
 	file.close();
 	return 0;
